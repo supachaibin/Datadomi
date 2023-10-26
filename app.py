e9ea166baa839dc7922ec74d9006b2f1e706c450
polonimu <90739985+polonimu@users.noreply.github.com>
2023-10-26 13:42:35 +0700

    V2 added main routing to the pages
    I haven't added the billadd page for some reasons, go figure it out

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/datadomi/home")
 def home():
-    """home"""
-    return "welcome to Datadomi, test basic"
+    return render_template("index.html")
+
+@app.route("/datadomi/rent")
+def rent():
+    return render_template("Rent.html")
+
+@app.route("/datadomi/addbill")
+def add_bill():
+    return render_template("addbill.html")
+
+@app.route("/datadomi/addmember")
+def add_member():
+    return render_template("add-member.html")
+
+@app.route("/datadomi/addroom")
+def add_room():
+    return render_template("add-room.html")
+
+@app.route("/datadomi/userM")
+def user_M():
+    return render_template("userM.html")
+
+@app.route("/datadomi/bill")
+def bill():
+    return render_template("bill.html")
+
+@app.route("/datadomi/adduser")
+def add_user():
+    return render_template("adduser.html")
